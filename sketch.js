5e313ab72986347bea6a7b4af32b871e97eb8e97
sathwik20 <75615582+sathwik20@users.noreply.github.com>
2021-03-25 13:07:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var ball;
+
+var database;
+function setup(){
+database=firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    var ballposition=database.ref('ball/position')
+    ballposition.on("value",function(data){
+        console.log(data.val())
+    })
+
+}
+
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
